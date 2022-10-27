569f4324cf7f25626e4ae07324c09ec024536670
AnnaSolozobova <solozobovaanna5@gmail.com>
2022-10-28 00:41:21 +0300

    coverage 100%

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -14,4 +14,16 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void IfTheHighestIsNotTheFirst() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 16, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 16;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
